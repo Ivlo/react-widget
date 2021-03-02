f65128e39077771f5d5af0aa4585bfe0de30f406
ivlo <ivan.lopez@starzplayarabia.com>
2021-03-02 20:28:45 +0100

    integrate widget in App.js

--- widget/src/App.js
+++ widget/src/App.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from "react";
-import Title from "./Title";
-import { getCreditAgreements } from "./services/creditAgreement";
-import baseConfig from "./baseConfig";
+import React from "react";
+import Widget from "./components/Widget";
 
 const App = () => {
-  const [data, setData] = useState(undefined);
-
-  const { creditAgreementUrl } = baseConfig.servicesUrl;
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getCreditAgreements(creditAgreementUrl);
-      setData(result);
-    };
-    fetchData();
-  }, [creditAgreementUrl]);
-
-  console.log(data);
-
   return (
     <>
-      <Title size="small">Hellow world!!</Title>
+      <Widget />
     </>
   );
 };
