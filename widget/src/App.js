47b483e82d99af3b75cafdfdf1271c4741a86be8
ivlo <ivan.lopez@starzplayarabia.com>
2021-03-02 10:45:05 +0100

    setup project include api widget react app and merchant-static site

---
+++ widget/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./Title";
+
+const App = () => {
+  return (
+    <>
+      <Title size="small">Hellow world!!</Title>
+    </>
+  );
+};
+
+export default App;
