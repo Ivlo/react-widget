4ba09d032c2fdde09b698a9319411780fe28bb40
ivlo <ivan.lopez@starzplayarabia.com>
2021-03-07 17:53:31 +0100

    test implementation for Modal component

--- widget/src/test/Modal.test.js
+++ widget/src/test/Modal.test.js
@@ -2,11 +2,32 @@ import React from "react";
 import { shallow } from "enzyme";
 import Modal from "../components/Modal";
 
-const children = "children component";
-const wrapper = shallow(<Modal>{children}</Modal>);
+const callbackClose = jest.fn();
+const children = "Content Modal";
 
 describe("Modal", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Modal onClickClose={callbackClose}>{children}</Modal>);
+  });
+
   it("should render the Modal Component correctly", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("the content of our Modalcontent element should be our children prop", () => {
+    const modalContent = wrapper.find(".modal__content");
+    expect(modalContent.text()).toEqual(children);
+  });
+
+  it("should be render with a close element", () => {
+    const modalAction = wrapper.find(".modal__action");
+    expect(modalAction.length).toBe(1);
+  });
+
+  it("close action callback should be execute when click on modal__action element", () => {
+    const modalAction = wrapper.find(".modal__action");
+    modalAction.simulate("click");
+    expect(callbackClose).toHaveBeenCalled();
   });
 });
